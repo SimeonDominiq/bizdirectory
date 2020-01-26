b8ce29366dc8746825ff00dfeb9d78b67edf4dad
Opeyemi Adeyeye <simeondominiq@gmail.com>
2020-01-26 01:13:10 +0100

    Modified DB connect details

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'id12359957_business_listing'),
             'username' => env('DB_USERNAME', 'id12359957_biz_list'),
